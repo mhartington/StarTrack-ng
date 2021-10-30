3e217eb7fc60906a87769c2ae08ed3bc091c2157
Mike Hartington <mikehartington@gmail.com>
2021-10-29 20:05:04 -0400

    chore(): remove unused things

--- src/@types/colorthief.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'colorthief' {
-  type Color = [number, number, number];
-  export default class ColorThief {
-    getColor: (img: HTMLImageElement | null) => Color;
-    getColorFromUrl: (imgSrc: string | null) => Color;
-    getPalette: (img: HTMLImageElement | null) => Color[];
-  }
-}
