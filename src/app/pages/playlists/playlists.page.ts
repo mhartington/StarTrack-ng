7bc042d1a1c4846e3544c9ad6b921949a9f5e11b
Mike Hartington <mikehartington@gmail.com>
2021-12-17 15:17:09 -0500

    update sharing

--- src/app/pages/playlists/playlists.page.ts
+++ src/app/pages/playlists/playlists.page.ts
@@ -1,18 +1,16 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { catchError, switchMap, map, switchMapTo } from 'rxjs/operators';
-import { Album } from '../../../@types/album';
 import { MusickitService } from '../../providers/musickit-service/musickit-service.service';
 import { PlayerService } from '../../providers/player/player.service2';
 import { Observable, of, Subject } from 'rxjs';
 import { RxState } from '@rx-angular/state';
 import { mapToError, mapToAlbumResults } from '../../util/fetchUtils';
+import { Playlist } from 'src/@types/playlist';
 
-interface IAlbumPageState {
-  collection: Partial<Album>;
-  isLoading: boolean;
-  hasError: boolean;
-  canShare: boolean;
+interface IPlaylistPage {
+  collection: Partial<Playlist>; isLoading: boolean;
+  hasError: boolean; canShare: boolean;
 }
 @Component({
   selector: 'app-playlist',
@@ -21,11 +19,9 @@ interface IAlbumPageState {
   providers: [RxState],
 })
 export class PlaylistPage {
-  public state$: Observable<IAlbumPageState> = this.stateService.select();
+  public state$: Observable<IPlaylistPage> = this.stateService.select();
   private ionViewDidEnter$ = new Subject<boolean>();
-
-  private fetchDataStream$ = this.route.params.pipe(
-    switchMap(({id }) => this.api.fetchPlaylist(id)),
+  private fetchDataStream$ = this.route.params.pipe( switchMap(({id }) => this.api.fetchPlaylist(id)),
     map(mapToAlbumResults),
     catchError((e) => of(mapToError(e)))
   );
@@ -34,11 +30,9 @@ export class PlaylistPage {
     private api: MusickitService,
     private route: ActivatedRoute,
     private player: PlayerService,
-    private stateService: RxState<IAlbumPageState>
-  ) {
-    this.stateService.set({
-      isLoading: true,
-      hasError: false,
+    private stateService: RxState<IPlaylistPage>
+  ) { this.stateService.set({
+      isLoading: true, hasError: false,
       collection: null,
     });
 
@@ -62,7 +56,7 @@ export class PlaylistPage {
       (navigator as any)
         .share({
           title: 'Star Track',
-          text: `Check out "${collection.attributes.name}" by ${collection.attributes.artistName}. Via Star Track.`,
+          text: `Check out "${collection.attributes.name}" by ${collection.attributes.curatorName}. Via Star Track.`,
           url: `${window.location.origin}/album/${collection.id}`,
         })
         .then(
